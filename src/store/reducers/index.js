4a3c1e5f17bd1830f8c29f9fe0042a31e2aed3bc
jm-avila <jm.avila1309@icloud.com>
2020-09-24 14:18:11 +0200

    Added empty store, with actions and redicerds per view

---
+++ src/store/reducers/index.js
@@ -0,0 +1,10 @@
+import { combineReducers } from "redux";
+import authentication from "./authentication";
+import userList from "./userList";
+import userDetails from "./userDetails";
+
+export default combineReducers({
+  authentication,
+  userList,
+  userDetails,
+});
---
+++ src/store/reducers/userList.js
@@ -0,0 +1,8 @@
+const INITIAL_STATE = null;
+
+export default (state = INITIAL_STATE, action) => {
+  switch (action.type) {
+    default:
+      return state;
+  }
+};
