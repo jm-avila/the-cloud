3a4d201dfb2b26b5a0b6bc8a78177239f40255a5
jm-avila <jm.avila1309@icloud.com>
2020-09-24 14:01:31 +0200

    Added Navigation for UsersList and UserDetails components

--- src/components/Navigation.jsx
+++ src/components/Navigation.jsx
@@ -2,7 +2,19 @@ import React from "react";
 import { NavLink } from "react-router-dom";
 
 function Navigation() {
-  return <nav></nav>;
+  return (
+    <nav>
+      <NavLink exact to="/">
+        Home
+      </NavLink>
+      <NavLink exact to="/users-list">
+        Users List
+      </NavLink>
+      <NavLink exact to="/user-details">
+        User Details
+      </NavLink>
+    </nav>
+  );
 }
 
 export default Navigation;
--- src/components/RoutesSwitch.jsx
+++ src/components/RoutesSwitch.jsx
@@ -1,10 +1,14 @@
 import React from "react";
 import { Route, Switch, Redirect } from "react-router-dom";
+import UsersList from "./UsersList";
+import UserDetails from "./UserDetails";
 
 function RoutesSwitch() {
   return (
     <div>
       <Switch>
+        <Route exact path="/users-list" component={UsersList} />
+        <Route exact path="/user-details" component={UserDetails} />
         <Redirect exact to="/" />
       </Switch>
     </div>
