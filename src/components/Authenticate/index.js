955afcb175b6db098d27026a2f4abbee39cd35bd
jm-avila <jm.avila1309@icloud.com>
2020-09-24 16:54:51 +0200

    Added UserDetails basic behaviour

--- src/components/Authenticate/index.js
+++ src/components/Authenticate/index.js
@@ -2,7 +2,7 @@ import React, { useState } from 'react';
 import Login from './Login';
 
 function Authenticate({ children }) {
-  const [authenticated, setAuthenticated] = useState(false);
+  const [authenticated, setAuthenticated] = useState(true);
   if (authenticated) return children;
   return <Login />;
 }
--- src/components/Views/UserDetails/index.js
+++ src/components/Views/UserDetails/index.js
@@ -1,9 +1,44 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import { TextInput, SimpleBtn } from '../../BaseComponents';
+import { NavLink } from 'react-router-dom';
 import dummyData from '../data.json';
 
 function UserDetails({ match }) {
   const userId = match.params.id;
-  return <div>User {userId} Details</div>;
+
+  const [firstName, setFirstName] = useState('');
+  const [lastName, setLastName] = useState('');
+  const [email, setEmail] = useState('');
+
+  useEffect(() => {
+    const currentUser = dummyData.data.find(({ id }) => id == userId);
+    if (currentUser) {
+      const { first_name, last_name, email } = currentUser;
+      setFirstName(first_name);
+      setLastName(last_name);
+      setEmail(email);
+    }
+  }, [userId]);
+
+  function handleUpdate() {
+    console.log('update');
+  }
+
+  function handleDelete() {
+    console.log('delete');
+  }
+
+  return (
+    <div>
+      <p>User {userId} Details</p>
+      <TextInput label="Nombre" value={firstName} onChange={setFirstName} />
+      <TextInput label="Apellido" value={lastName} onChange={setLastName} />
+      <TextInput type="email" label="Email" value={email} onChange={setEmail} />
+      <SimpleBtn text={'Actualizar'} onClick={handleUpdate} />
+      <SimpleBtn text={'Borrar'} onClick={handleDelete} />
+      <NavLink to="/detalle">Volver</NavLink>
+    </div>
+  );
 }
 
 export default UserDetails;
