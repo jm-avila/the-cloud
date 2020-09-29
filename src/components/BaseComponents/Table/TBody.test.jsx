cc8b18f7afefb517e567c85a9fd01e47b07a29b3
jm-avila <jm.avila1309@icloud.com>
2020-09-29 15:27:30 +0200

    Corrected description

--- src/components/BaseComponents/Table/TBody.test.jsx
+++ src/components/BaseComponents/Table/TBody.test.jsx
@@ -4,7 +4,7 @@ import { render } from '@testing-library/react';
 import '@testing-library/jest-dom/extend-expect';
 
 describe('TBody render test.', () => {
-  test('shoudl render an tbody element; given an array of arrays, each array element will be mapped as a tr element and its values, arrays, will be mapped as td elements', () => {
+  test('should render an tbody element; given an array of arrays as the column prop, each array element will be mapped as a tr element and its values, arrays, will be mapped as td elements', () => {
     const rows = [
       ['row1-col1', 'row1-col2', 'row1-col3'],
       ['row2-col1', 'row2-col2', 'row2-col3'],
