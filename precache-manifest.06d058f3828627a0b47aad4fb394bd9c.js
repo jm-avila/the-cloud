f662e2d64b2648aa584521865371b5cd114a9ac8
jm-avila <jm.avila1309@icloud.com>
2020-10-13 15:43:14 +0200

    Updates

---
+++ precache-manifest.06d058f3828627a0b47aad4fb394bd9c.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "39d776639c0ac61f87a26434076783ef",
+    "url": "/the-cloud/index.html"
+  },
+  {
+    "revision": "89bf151e752c89995206",
+    "url": "/the-cloud/static/css/main.f31aa989.chunk.css"
+  },
+  {
+    "revision": "6796de37013e17d2513e",
+    "url": "/the-cloud/static/js/2.d30e3813.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/the-cloud/static/js/2.d30e3813.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "89bf151e752c89995206",
+    "url": "/the-cloud/static/js/main.a5a3e4fc.chunk.js"
+  },
+  {
+    "revision": "fb275ffe449220361428",
+    "url": "/the-cloud/static/js/runtime-main.f03aed4d.js"
+  },
+  {
+    "revision": "6ec953f2370433c6443c8071f0e226c1",
+    "url": "/the-cloud/static/media/clouds.6ec953f2.jpg"
+  }
+]);
